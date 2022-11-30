be7d7d2801d3c2eb9989890b6ad61975d8ac1210
neil dela cruz <genoznightmare@gmail.com>
2022-11-30 13:06:52 +0800

    created jwt createToke

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 const User = require("../models/userModel");
 const { validationResult } = require("express-validator");
+const createToken = require("../auth/createToken");
 
 const loginUser = async (req, res) => {
   res.json({ message: "login user" });
@@ -15,7 +16,8 @@ const signUpUser = async (req, res) => {
 
   try {
     const user = await User.signup(email, password);
-    res.status(200).json({ email, user });
+    const token = createToken(user._id);
+    res.status(200).json({ email, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
